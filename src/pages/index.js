b8fd82644217453a662e876d8bae19253306fd2e
Antoine Despres <antoinedespres13@gmail.com>
2024-04-17 11:13:34 +0200

    :alembic: try themed image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -13,7 +13,13 @@ function HomepageHeader() {
     <header className={clsx('hero hero--secondary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          <img src="img/logo.png" alt="Certy Logo" />
+        <ThemedImage
+          alt="Docusaurus themed image"
+          sources={{
+            light: useBaseUrl('/img/logo_black.png'),
+            dark: useBaseUrl('/img/logo_white.png'),
+          }}
+        />;
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
